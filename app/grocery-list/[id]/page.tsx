cc86ccee8f18fd6e01af2814a0433f6029fe58d8
Haman Malova <harmanmalova@gmail.com>
2025-06-17 15:00:53 +0300

    pushing piano

--- app/grocery-list/[id]/page.tsx
+++ app/grocery-list/[id]/page.tsx
@@ -23,20 +23,10 @@ export default function GroceryListPage() {
 
   return (
    
-<div className="min-h-screen bg-gray-50">
-<header className="bg-white shadow">
-  <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-    <h1 className="text-3xl font-bold text-gray-900">Grocery List</h1>
-  </div>
-</header>
-<main>
-  <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+
     <Suspense fallback={<LoadingSkeleton />}>
         <GroceryList id={id}  />
     </Suspense>
-  </div>
-</main>
-</div>
 )
   
 }
