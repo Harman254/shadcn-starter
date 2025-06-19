783f70a0240af9da2ba8552ed4a0b30c78edcdfa
Haman Malova <harmanmalova@gmail.com>
2025-06-19 14:59:59 +0300

    resend

--- app/grocery-list/[id]/grocery-list-client.tsx
+++ app/grocery-list/[id]/grocery-list-client.tsx
@@ -9,15 +9,6 @@ interface GroceryListClientProps {
 }
 
 export default function GroceryListClient({ id }: GroceryListClientProps) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  console.log('Current pathname:', pathname);
-  const lat = searchParams.get('lat');
-  const lon = searchParams.get('lon');
-  
-  console.log('Extracted id:', id);
-  console.log('Extracted latitude:', lat);
-  console.log('Extracted longitude:', lon);
 
   if (!id) {
     return <div className="text-red-500">Error: No ID provided in the URL.</div>;
